25d16012b2cea4e7e303fe2772e8825d011b4d18
Simon <simon.tan09@gmail.com>
2023-04-05 23:00:54 -0700

    first commit

---
+++ GraphQL-APIs/app.py
@@ -0,0 +1,41 @@
+# Importing Flask to be our HTTP server
+
+from flask import Flask
+
+# Importing GraphQLView
+
+from graphql_server.flask import GraphQLView
+
+from schema import schema
+
+# Importing this view so Flask can delegate to Graphene
+
+app=Flask(__name__)
+
+
+# Adding a single route for /
+
+@app.route("/")
+
+def young_ben():
+    return "Hello There"
+
+# Adding another route for GraphQL at /graphql
+
+app.add_url_rule(
+
+'/graphql',
+
+view_func=GraphQLView.as_view(
+
+     'graphql',
+
+     schema=schema,
+
+     graphiql=True
+
+)
+
+)
+
+curl -X POST \ -H 'Content-Type: application/json' \ -d '{"query":"query { who(name:\"Luke\") }"}' \
---
+++ GraphQL-APIs/schema.py
@@ -0,0 +1,20 @@
+# Importing Graphene so we can define a simple GraphQL schema
+
+import graphene
+
+
+class Query(graphene.ObjectType):
+
+# defining a field "who" in the schema with the argument "name"`
+
+    who = graphene.String(name=graphene.String(default_value="Leia"))
+
+
+# This returns the GraphQL context (root, info) and adds the argument
+
+def resolve_who(root, info, name):
+
+     return f'Hi {name}!'
+
+
+schema = graphene.Schema(query=Query)
